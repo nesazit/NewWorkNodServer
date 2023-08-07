5e1726e01b3f4a6e8db091e80c01ab3fec893982
nesazit <nesazit@gmail.com>
2023-08-07 11:48:31 +0330

    main

--- index.js
+++ index.js
@@ -21,6 +21,26 @@ const connect = () => {
     });
 };
 
+//* first method
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+//* second method
+// import cors from "cors";
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
